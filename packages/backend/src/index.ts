d4c14a9049eff3de15e8e6fbf792d96b3cb0c647
Arpit Pandey <Arpit.Pandey@talentica.com>
2025-01-16 12:34:00 +0530

    demo part ready

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,19 +1,25 @@
-/*
- * Hi!
- *
- * Note that this is an EXAMPLE Backstage backend. Please check the README.
- *
- * Happy hacking!
- */
-
 import { createBackend } from '@backstage/backend-defaults';
+import weatherApi from './api/weatherApi';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use('/api/weather', weatherApi);
+
+console.log('Using PORT:', process.env.PORT);
+const port = process.env.PORT || 7008; // Fallback to a default port
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 const backend = createBackend();
 
 backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
-backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
@@ -28,6 +34,7 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
