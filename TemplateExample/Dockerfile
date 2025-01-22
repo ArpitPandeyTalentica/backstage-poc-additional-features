1d5da44f4a34f1932e42fad3eddb01d8e241eabe
Arpit Pandey <Arpit.Pandey@talentica.com>
2025-01-22 14:56:01 +0530

    Cleaned and added templates

---
+++ TemplateExample/Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:11-alpine
+ENTRYPOINT ["/usr/bin/hello-world.sh"]
+
+COPY hello-world.sh /usr/bin/hello-world.sh
+COPY target/hello-world.jar /usr/share/hello-world/hello-world.jar
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -59,5 +59,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 
 backend.start();
